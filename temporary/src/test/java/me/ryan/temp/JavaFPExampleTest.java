774d526c1805e43077dbe30c8ce98111b9a5b684
mogongzi <superrenyuan@hotmail.com>
2020-12-27 12:43:42 +0800

    add java 8 FP Supplier<T> and Predicate<T> examples

--- temporary/src/test/java/me/ryan/temp/JavaFPExampleTest.java
+++ temporary/src/test/java/me/ryan/temp/JavaFPExampleTest.java
@@ -2,7 +2,13 @@ package me.ryan.temp;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.function.DoubleSupplier;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class JavaFPExampleTest {
@@ -37,5 +43,72 @@ public class JavaFPExampleTest {
         Consumer<String> printConsumer = System.out::println;
         Stream<String> cities = Stream.of("Sydney", "Dhaka", "New York", "London");
         cities.forEach(printConsumer);
+    }
+
+    /**
+     * the usage of composing multiple consumer implementations to make a chain of consumers
+     */
+    @Test
+    public void whenNamesPresentUseBothConsumer() {
+        List<String> cities = Arrays.asList("Sydney", "Dhaka", "New York", "London");
+        Consumer<List<String>> upperCaseConsumer = list -> {
+            for (int i = 0; i < list.size(); i++) {
+                list.set(i, list.get(i).toUpperCase());
+            }
+        };
+
+        Consumer<List<String>> printConsumer = list -> {
+          list.forEach(System.out::println);
+        };
+
+        upperCaseConsumer.andThen(printConsumer).accept(cities);
+    }
+
+    @Test
+    public void supplier() {
+        /*
+         * Supplier<Double> doubleSupplier1 = new Supplier<Double>() {
+            @Override
+            public Double get() {
+                return Math.random();
+            }
+        };
+        *
+        * anonymous class that implements the Supplier interface and overwrite
+        * get method
+        */
+
+        Supplier<Double> doubleSupplier1 = () -> Math.random();
+        DoubleSupplier doubleSupplier2 = Math::random;
+
+        System.out.println(doubleSupplier1.get());
+        System.out.println(doubleSupplier2.getAsDouble());
+    }
+
+    @Test
+    public void supplierWithOptional() {
+        Supplier<Double> doubleSupplier = Math::random;
+        Optional<Double> optionalDouble = Optional.empty();
+        System.out.println(optionalDouble.orElseGet(doubleSupplier));
+    }
+
+    /**
+     * A Predicate interface represents a boolean-valued-function of an argument. This is mainly used to filter data from a Java Stream.
+     */
+    @Test
+    public void testPredicate() {
+        List<String> names = Arrays.asList("John", "Smith", "Samuel", "Cathy", "Sie");
+
+        /*
+         * Predicate<String> nameStartsWith = new Predicate<String>() {
+            @Override
+            public boolean test(String s) {
+                return s.startsWith("S");
+            }
+          };
+         */
+        Predicate<String> nameStartsWith = s -> s.startsWith("S");
+
+        names.stream().filter(nameStartsWith).forEach(System.out::println);
     }
 }
