ad204d6dea2e2e4db1a18cc21cc79bb8fd8d54cf
mogongzi <superrenyuan@hotmail.com>
2021-02-15 12:42:23 +0800

    use logger instead of system.out

--- temporary/src/main/java/me/ryan/temp/IndexOfExample.java
+++ temporary/src/main/java/me/ryan/temp/IndexOfExample.java
@@ -1,12 +1,17 @@
 package me.ryan.temp;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class IndexOfExample {
+
+    public static final Logger log = LogManager.getLogger(IndexOfExample.class);
 
     public static void main(String[] args) {
 
         String str1 = "hello";
         String str2 = "hel";
 
-        System.out.println(str1.indexOf(str2));
+        log.info(str1.indexOf(str2));
     }
 }
