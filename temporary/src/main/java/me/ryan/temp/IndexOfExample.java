bb26690f282dc2e78d8aa29d9d8de31f20bfbb3b
mogongzi <superrenyuan@hotmail.com>
2021-02-15 12:40:55 +0800

    remove general module and still use temp

---
+++ temporary/src/main/java/me/ryan/temp/IndexOfExample.java
@@ -0,0 +1,12 @@
+package me.ryan.temp;
+
+public class IndexOfExample {
+
+    public static void main(String[] args) {
+
+        String str1 = "hello";
+        String str2 = "hel";
+
+        System.out.println(str1.indexOf(str2));
+    }
+}
