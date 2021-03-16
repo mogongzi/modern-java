fcbbda50dc5befc58d75b17a318dde4c92693599
mogongzi <superrenyuan@hotmail.com>
2021-03-16 16:13:43 +0800

    replace system.out by logger

--- classic-cs-problems/src/main/java/em/ryan/algorithm/FibonacciWithStream.java
+++ classic-cs-problems/src/main/java/em/ryan/algorithm/FibonacciWithStream.java
@@ -1,8 +1,13 @@
 package em.ryan.algorithm;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.stream.IntStream;
 
 public class FibonacciWithStream {
+
+    public static final Logger log = LogManager.getLogger(FibonacciWithStream.class);
 
     private int last = 0, next = 1; // fib(0), fib(1)
 
@@ -17,6 +22,6 @@ public class FibonacciWithStream {
 
     public static void main(String[] args) {
         FibonacciWithStream fib = new FibonacciWithStream();
-        fib.stream().limit(41).forEachOrdered(System.out::println);
+        fib.stream().limit(41).forEachOrdered(log::info);
     }
 }
